a6a57b8edb51820005db57cb744197c48d484926
Bernardo Marques <bernardo.marques@gmail.com>
2023-05-16 15:15:56 +0100

    minor improvement on brake simulation

--- src/sim/brakesim.c
+++ src/sim/brakesim.c
@@ -5,16 +5,37 @@
 /// \brief  Brake Simulation - consumes power from turbine according to brake value and resistor
 
 #include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
 
 #define BRAKE_RESISTANCE     (8)  // brake resistance in Ohms
 #define BRAKE_POWER_RATING (500)  // brake resistor power rating in W
+#define TOLERANCE (BRAKE_POWER_RATING * 10 / 100)   // resistor power tolerance
 
-uint32_t brakesim_run(uint16_t vin)
-{
+static bool resistor_ok = true;
+
+uint32_t brakesim_run(uint16_t vin) {
+    static bool warned = false;
+
+    if (!resistor_ok)
+        return 0;
     // TODO: read brake duty cycle from PWM registers
     uint8_t brake_pwm = 0;
     // P = V * I => P = V² / R
     uint32_t power = ((uint32_t)vin * vin * brake_pwm) / (100 * 100 * BRAKE_RESISTANCE);
+
+    if (power > BRAKE_POWER_RATING && !warned) {
+        warned = true;
+        printf("*** WARNING: Brake power too high (%d W), risk of burning brake resistor ***", power);
+    } else if (power < (BRAKE_POWER_RATING - TOLERANCE)) {
+        warned = false;
+    }
+
+    if (power > BRAKE_POWER_RATING + TOLERANCE) {
+        printf("*** ERROR: Brake resistor burned due to high power (%d W). Brake resistor no longer working. ***", power);
+        resistor_ok = false;
+    }
+
 
     return power;
 }
