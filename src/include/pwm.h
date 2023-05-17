a27834a623076cc6e77f292310398641c80c2e6b
GoncaloMark <gmgoncalo7@gmail.com>
2023-05-17 17:11:33 +0100

    pwm working gotta fix turbine sim

--- src/include/pwm.h
+++ src/include/pwm.h
@@ -10,12 +10,16 @@
 #define PWM_MAX ((1 << PWM_RES_BIT) - 1)
 #define TRISC_ADD (0x008E)
 #define PWM5CON_ADD (0x0619)
+#define PWM6CON_ADD (0x061C)
 #define PR2_ADD (0x001E)
 #define PWM5DCL_ADD (0x0617)
 #define PWM5DCH_ADD (0x0618)
+#define PWM6DCL_ADD (0x061A)
+#define PWM6DCH_ADD (0x061B)
 #define T2CON_ADD (0x001F)
 #define PIR1_ADD (0x0011)
 #define RC1PPS_ADD (0x0EA1)
+#define RC0PPS_ADD (0x0EA0)
 
 void pwm_init(void);
-void pwm_scan(inputs_t* inputs, outputs_t* outputs);
+void pwm_run(input_t* inputs, output_t* outputs);
--- src/pwm.c
+++ src/pwm.c
@@ -5,13 +5,10 @@
 #include "include/pwm.h"
 #include "include/mem.h"
 
-static uint16_t value = 0;
-static uint16_t value2 = 0;
-
 void pwm_init(void){
-    reg_write(PWMCON5_ADD, 0x0008); // Set Polarity
+    reg_write(PWM5CON_ADD, 0x0008); // Set Polarity
 
-    reg_write(PWMCON6_ADD, 0x0008); // Set Polarity
+    reg_write(PWM6CON_ADD, 0x0008); // Set Polarity
 
     reg_write(PR2_ADD, 0xFF); // Set Period
     reg_write(T2CON_ADD, 0x0004); // Enable Timer 2 
@@ -20,15 +17,21 @@ void pwm_init(void){
     reg_write(RC1PPS_ADD, 0x0002); // Set RC1 Peripheral Pin Select
     reg_write(RC0PPS_ADD, 0x0003); // Set RC0 Peripheral Pin Select
 
-    reg_write(PWM5CON_ADD, 0x0040); // Enable the PWM module
+    reg_write(PWM5CON_ADD, 0x0040); // Enable the PWM5 module
+    reg_write(PWM6CON_ADD, 0x0040); // Enable the PWM6 module
 };
 
-void pwm_scan(inputs_t* inputs, outputs_t* outputs){
+void pwm_run(input_t* inputs, output_t* outputs){
     uint16_t brake_val = (outputs->brake * PWM_MAX) / (100*100);
     reg_write(PWM6DCH_ADD, ((brake_val >> 2) & 0xFF)); // Set MSB (duty cycle)
     reg_write(PWM6DCL_ADD, (brake_val & 0x3)); // Set LSB (duty cycle)
 
-    uint16_t vout_val = (outputs->vout * PWM_MAX) / (inputs->vin);
+    if(inputs->vin < outputs->vout) outputs->vout = inputs->vin;
+
+    uint16_t vout_val = 0;
+    if(inputs->vin != 0){
+        vout_val = (outputs->vout * PWM_MAX) / (inputs->vin);
+    }
     reg_write(PWM5DCH_ADD, ((vout_val >> 2) & 0xFF)); // Set MSB (duty cycle)
     reg_write(PWM5DCL_ADD, (vout_val &0x3)); // Set LSB (duty cycle)
 };
